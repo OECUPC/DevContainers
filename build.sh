6a304019a3338cb743ca8aa51931f0ac7eee2b76
Minamin1234 <minamin37345@gmail.com>
2023-08-12 00:50:33 +0900

    add: conding container

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 docker container stop $1
 docker rm $1
 docker image build -t $1 .
-docker container run -it -d --name $1 $1
+docker container run \
+    --mount type=bind,src=$(pwd),dst=/home/Apps/$1 \
+    -it \
+    -d \
+    --name $1 $1
+docker container exec -it $1 /bin/bash
